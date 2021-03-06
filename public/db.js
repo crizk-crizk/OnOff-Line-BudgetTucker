a1efc952f7d1ea30e6601a7b7a3b2fdbcbe61c0e
Carolina R <carolinarizk@gmail.com>
2021-03-06 17:00:52 -0500

    added saveRecord and checkDatabase functions

--- public/db.js
+++ public/db.js
@@ -12,9 +12,43 @@ request.onsuccess = function (event) {
   //navigator.onLine returns true/false based on whether application is on line or not. If not online, this function will not be checked.
   if (navigator.onLine) {
     //call a function that checks the offline database
+    checkDatabase();
   }
 };
 
 request.onerror = function (event) {
   console.error("error: ", request.error);
 };
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const pendingStore = transaction.objectStore("pending");
+  pendingStore.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const pendingStore = transaction.objectStore("pending");
+  const getAll = pendingStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const pendingStore = transaction.objectStore("pending");
+          pendingStore.clear();
+        });
+    }
+  };
+}
+
+window.addEventListener("online", checkDatabase);
